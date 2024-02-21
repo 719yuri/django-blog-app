ffbc87f1c351ec351ba6a0fc3d19ac4fe69a96c9
719yuri <yuri.tennis1205@gmail.com>
2024-02-21 21:40:24 +0900

    ブログ公開設定のカラム

--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-  title = models.CharField(max_length = 200)
-  content = models.TextField
-  create_at = models.DateTimeField
-  update_at = models.DateTimeField
-  is_published = models.BooleanField #wakaranu~~~!
+  title = models.CharField('タイトル', max_length = 200)
+  content = models.TextField()
+  create_at = models.DateTimeField('更新日時', auto_now=True)
+  update_at = models.DateTimeField('更新日時', auto_now=True)
+  is_published = models.BooleanField
